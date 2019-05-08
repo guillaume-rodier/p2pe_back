727b04a8f9965ec0f422610554bd7be8954a0457
yaz <younes.azoum@epitech.eu>
2019-05-08 17:02:03 +0200

    Init of the v1 API, users management, jwt, basic config done

---
+++ index.js
@@ -0,0 +1,81 @@
+const config = require('./config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const users = require('./users')
+const app = express()
+const port = 3000
+
+//TODO: Externalize all the config stuff into config.js (port, DB and so on...).
+/* //Bycrypt
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+//TODO: Use that in order to create the inscription handler and modify the login route in order to compare hash (Watch out about the JWT management handlers)
+bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+      // Store hash in your password DB.
+  });
+});
+
+// Load hash from your password DB.
+bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
+  // res == true
+});
+ */
+//jwt stuff
+const secret = config.secret//normally stored in process.env.secret
+const jwt = require("jsonwebtoken");
+const opts = {}
+
+//passport stuff
+const passport = require("passport");
+const jwtStrategry  = require("./strategies/jwt")
+passport.use(jwtStrategry);
+
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+
+
+app.get('/users', passport.authenticate('jwt', { session: false }),users.getUsers) //TODO: Find a fancier way to secure the routes (maybe with a express router)
+app.get('/users/:id', users.getUserById)
+app.post('/users', users.createUser)
+app.put('/users/:id', users.updateUser)
+app.delete('/users/:id', users.deleteUser)
+
+
+app.post("/login", (req, res) => {
+  let { email, password } = req.body;
+  //This lookup would normally be done using a database
+  if (email === "aa") {
+      if (password === "aaa") { //Change the verification mechanism-> DB TODO: Check Bycrpt pour stocquer un hash et les comparer
+          opts.expiresIn = '24h';  //token expires in 2min
+          const token = jwt.sign({ email, password }, secret, opts);
+          return res.status(200).json({
+              message: "Auth Passed",
+              token
+          })
+      }
+  }
+  return res.status(401).json({ message: "Auth Failed" })
+});
+
+app.get("/protected", passport.authenticate('jwt', { session: false }), (req, res) => {
+  return res.status(200).send("YAY! this is a protected Route")
+})
+app.listen(3000, () => {
+  console.log(`App running on port ${port}.`)
+})
+
+
+
---
+++ users.js
@@ -0,0 +1,74 @@
+const pool = require('./db').pool
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users', (error, results) => {
+      if (error) {
+          response.status(400).send('Couldn t get the users') 
+        }
+      response.status(200).json(results.rows)
+    })
+}
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      response.status(400).send(`User added with ID: ${id}`)
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createUser = (request, response) => {
+  
+const { email,password,gender,address,phone_number,first_name,last_name,role,company_name,company_description,number_employee,siret,state,profession } = request.body
+
+pool.query('INSERT INTO users VALUES (default, $1, $2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14, now(), now()) RETURNING id',
+[email,password,gender,address,phone_number,first_name,last_name,role,company_name,company_description,number_employee,siret,state,profession], 
+(error, results) => {
+    if (error) {
+      console.log(error)
+
+      response.status(400).send(`Could not create the user with the provided data`)
+      return
+    }
+    response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+    return
+  })
+}
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        response.status(400).send(`Couldn't update the user with ID: &{id}`)
+      }
+      response.status(200).send(`User deleted with ID: ${id}`)
+    }
+  )
+}
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      response.status(400).send("Couldn't delete the user")
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+}

e51fde054907bb1514bdae14c82935d6a86b4ab8
tos9lm <guillaume.rodier@epitech.eu>
2019-09-10 23:02:17 +0200

    new branch

--- db.js
+++ db.js
@@ -8,24 +8,24 @@ const pool = new Pool({
 })
 
 const createTables = async () => {
-  
- await pool.query('CREATE TABLE IF NOT EXISTS users (id Serial PRIMARY KEY,email VARCHAR UNIQUE,password VARCHAR,gender VARCHAR,address VARCHAR,phone_number VARCHAR UNIQUE,first_name VARCHAR,last_name VARCHAR,role VARCHAR,company_name VARCHAR,company_description VARCHAR,number_employee INTEGER,siret VARCHAR UNIQUE,state VARCHAR,profession VARCHAR)', (error, results) => {
+
+ await pool.query('CREATE TABLE IF NOT EXISTS users (id Serial PRIMARY KEY,email VARCHAR UNIQUE,password VARCHAR,gender VARCHAR,address VARCHAR,phone_number VARCHAR UNIQUE,first_name VARCHAR,last_name VARCHAR,role VARCHAR,company_name VARCHAR,company_description VARCHAR,number_employee INTEGER,siret VARCHAR UNIQUE,state VARCHAR,profession VARCHAR,image_screen TEXT)', (error, results) => {
     if (error) {
-        console.log('Cannot create tables users : ' + error.message) 
+        console.log('Cannot create tables users : ' + error.message)
         return
       }
     console.info('table users created')
     pool.query('CREATE TABLE IF NOT EXISTS proposed_services (id SERIAL, name VARCHAR, description VARCHAR, location VARCHAR, price FLOAT, creation_date timestamp, state VARCHAR, rate FLOAT, option VARCHAR, id_pro SERIAL, PRIMARY KEY (id), FOREIGN KEY (id_pro) REFERENCES users(id))', (error, results) => {
       if (error) {
-          console.log('Cannot create tables proposed_services : ' + error) 
+          console.log('Cannot create tables proposed_services : ' + error)
           return
         }
       console.log('table proposed_services created')
 
     pool.query('CREATE TABLE IF NOT EXISTS requested_services (id SERIAL, state VARCHAR, paid VARCHAR, creation_date timestamp, address VARCHAR, expiration_date timestamp, id_user SERIAL, id_proposed SERIAL, PRIMARY KEY (id), FOREIGN KEY (id_user) REFERENCES users(id), FOREIGN KEY (id_proposed) REFERENCES proposed_services(id))', (error, results) => {
       if (error) {
-        console.log('Cannot create tables requested_services : ' + error.message, results) 
-        return   
+        console.log('Cannot create tables requested_services : ' + error.message, results)
+        return
       }
       console.log('table requested_services created')
     })
@@ -33,11 +33,11 @@ const createTables = async () => {
   })
  }
 
- 
+
 const dropTables = () => {
   pool.query('DROP TABLE IF EXISTS users', (error, results) => {
     if (error) {
-        console.log('cannot drop tables : ' + error.message ) 
+        console.log('cannot drop tables : ' + error.message )
       }
     console.log(results)
   })
